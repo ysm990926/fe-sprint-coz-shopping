7cf4ec0bcfc0a7ccff15c03a611e24196ea3f107
YunSunMoon <ysm990926@gmail.com>
2023-07-12 13:04:03 +0900

    Footer

---
+++ coz-shop/src/components/Footer.js
@@ -0,0 +1,10 @@
+
+
+const Footer = () =>{
+    return <div className="footer">
+        <div>개인정보 처리방침 | 이용약관 </div>
+    <div>All rights reserved @ Codestates</div>
+    </div>
+}
+
+export default Footer
